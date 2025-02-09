51f7b2fbdfc2c55f41d0516e001f373a6eeb60c5
David Pechtalt <davidpechtalt@gmail.com>
2025-02-09 16:16:16 +0200

    4 chapter

--- flow-control/switch/begin/main.go
+++ flow-control/switch/begin/main.go
@@ -4,17 +4,29 @@ package main
 import (
 	"fmt"
 	"runtime"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
-	os := runtime.GOOS
+	// os := runtime.GOOS
 
 	// refactor with switch statement
-	if os == "linux" || os == "darwin" || os == "unix" {
+	// if os == "linux" || os == "darwin" || os == "unix" {
+	// 	fmt.Println("*nix variant")
+	// } else if os == "windows" {
+	// 	fmt.Println("Windows")
+	// } else {
+	// 	fmt.Printf("%s.\n", os)
+	// }
+	spew.Dump(runtime.GOOS)
+
+	switch os := runtime.GOOS; os{
+	case "linux", "darwin", "unix":
 		fmt.Println("*nix variant")
-	} else if os == "windows" {
+	case "windows":
 		fmt.Println("Windows")
-	} else {
+	default:
 		fmt.Printf("%s.\n", os)
 	}
 }
