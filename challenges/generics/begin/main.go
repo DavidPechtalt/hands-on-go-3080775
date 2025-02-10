9c7287a41fca1c13cd366343bcf842089dc3a9bb
David Pechtalt <davidpechtalt@gmail.com>
2025-02-10 15:49:12 +0200

    chapter 6

--- challenges/generics/begin/main.go
+++ challenges/generics/begin/main.go
@@ -4,43 +4,47 @@ package main
 import "fmt"
 
 // Part 1: print function refactoring
-
-// non-generic print functions
-
-func printString(s string) { fmt.Println(s) }
-
-func printInt(i int) { fmt.Println(i) }
-
-func printBool(b bool) { fmt.Println(b) }
+func print[T string | int | bool](input T) {
+	fmt.Println(input)
+}
 
 // Part 2 sum function refactoring
+type numeric interface {
+	~int | ~float64
+}
 
 // sum sums a slice of any type
-func sum(numbers []interface{}) interface{} {
-	var result float64
+// func sum(numbers []interface{}) interface{} {
+// 	var result float64
+// 	for _, n := range numbers {
+// 		switch n.(type) {
+// 		case int:
+// 			result += float64(n.(int))
+// 		case float32, float64:
+// 			result += n.(float64)
+// 		default:
+// 			continue
+// 		}
+// 	}
+// 	return result
+// }
+func sumAny[T numeric](numbers ...T) T {
+	var result T
 	for _, n := range numbers {
-		switch n.(type) {
-		case int:
-			result += float64(n.(int))
-		case float32, float64:
-			result += n.(float64)
-		default:
-			continue
-		}
+		result += n
 	}
 	return result
 }
 
 func main() {
-	// call non-generic print functions
-	printString("Hello")
-	printInt(42)
-	printBool(true)
 
 	// call generic printAny function for each value above
-
+	print("Hello")
+	print(42)
+	print(true)
 	// call sum function
-	fmt.Println("result", sum([]interface{}{1, 2, 3}))
+	// fmt.Println("result", sum([]interface{}{1, 2, 3}))
 
 	// call generics sumAny function
+	fmt.Println(sumAny(1, 2, 3,5.00000000000001, 3.3))
 }
--- generics/standard-library/begin/main.go
+++ generics/standard-library/begin/main.go
@@ -1,31 +1,22 @@
 // generics/standard-library/begin/main.go
 package main
 
-// create a numeric interface with a type set
-type numeric interface {
-	~int | ~float64
-	grow()
-}
-
-// update sum function to use a numeric interface with a type set
-func sum[T numeric](a, b T) T {
-	return a + b
-}
-
-type specialInt int
-
-func (s specialInt) grow() {}
+import (
+	"fmt"
+)
 
 // equal returns true if a and b are equal.
-//
+func equal[T comparable](a, b T) bool {
+	return a == b
+}
 
 func main() {
 	// invoke equal with comparable types
-	// fmt.Println("equal(1, 1):", equal(1, 1))
-	// fmt.Println("equal(\"one\", \"two\"):", equal("one", "two"))
+	fmt.Println("equal(1, 1):", equal(1, 1))
+	fmt.Println("equal(\"one\", \"two\"):", equal("one", "two"))
 
 	// invoke equal with a custom type
-	// type c struct{ f string }
-	// fmt.Println("equal(c{f: \"a\"}, c{f: \"a\"}):", equal(c{f: "a"}, c{f: "a"}))
-	// fmt.Println("equal(c{f: \"a\"}, c{f: \"b\"}):", equal(c{f: "a"}, c{f: "b"}))
+	type c struct{ f string }
+	fmt.Println("equal(c{f: \"a\"}, c{f: \"a\"}):", equal(c{f: "a"}, c{f: "a"}))
+	fmt.Println("equal(c{f: \"a\"}, c{f: \"b\"}):", equal(c{f: "a"}, c{f: "b"}))
 }
